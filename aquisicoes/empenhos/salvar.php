43ee94945dee3f45edcf8f1ad0e5d68929cfb644
Raphael73wx <raphael29wx@gmail.com>
2026-05-29 12:53:00 -0300

    att 290526

--- aquisicoes/empenhos/salvar.php
+++ aquisicoes/empenhos/salvar.php
@@ -6,44 +6,63 @@ include("../conexao-pdo.php");
 
 if ($_POST) {
     //verifica campos obrigatórios
-    if (empty($_POST["pk_empenhos"]) || empty($_POST["nome"])) {
+    if (empty($_POST["nome"])) {
         $_SESSION["tipo"] = 'warning';
         $_SESSION["title"] = 'Ops!';
         $_SESSION["msg"] = 'Por favor, preencha os campos obrigatórios.';
         header("location: ./");
         exit;
     } else {
-        $pk_pedidos = trim($_POST["pk_pedidos"]);
-        $pedido = trim($_POST["pedido"]);
-        $forma_p = trim($_POST["forma_p"]);
-        $endereco = trim($_POST["endereco"]);
-        $data_ini = trim($_POST["data_ini"]);
-        $data_fim = trim($_POST["data_fim"]);
-        $numero_p = trim($_POST["numero_p"]);
+        $pk_empenhos = trim($_POST["pk_empenhos"]);
+        $nome = trim($_POST["nome"]);
+        $setor = trim($_POST["setor"]);
+        $preco = trim($_POST["preco"]);
+        $cnpj = trim($_POST["cnpj"]);
+        $data_de_envio = trim($_POST["data_de_envio"]);
+        $nota_fiscal_num = trim($_POST["nota_fisca_num"]);
+        $financeiro_num = trim($_POST["financeiro_num"]);
+        $ordem_bancaria = trim($_POST["ordem_bancaria"]);
+        $valor_de_entrega = trim($_POST["valor_de_entrega"]);
+        $notificacoes_atraso = trim($_POST["notificacoes_atraso"]);
+        $qtd = trim($_POST["qtd"]);
+        $ordem_bancaria_num = trim($_POST["ordem_bancaria_num"]);
+        $financeiro = trim($_POST["financeiro"]);
+        $nota_fiscal = trim($_POST["nota_fiscal"]);
+        $requisicao_num = trim($_POST["requisicao_num"]);
+        $envio_emp = trim($_POST["envio_emp"]);
+        $codigo_empenho = trim($_POST["codigo_empenho"]);
+        $conclusao_emp = trim($_POST["conclusao_emp"]);
         try {
-            if (empty($pk_pedidos)) {
+            if (empty($pk_empenhos)) {
                 $sql = "
-             INSERT INTO pedidos (pedido,forma_de_pagamento,endereco_de_entrega,numero_do_pedido,data_ini,data_fim)
-             VALUES(:pedido,:forma_p,:endereco,:numero_p,CURDATE(),:data_fim)
+             INSERT INTO empenhos (nome,setor,cnpj,data_de_envio,nota_fiscal_num,financeiro_num)
+             VALUES(:nome,:setor,:cnpj,:data_de_envio,nota_fiscal_num,:financeiro_num)
              ";
-             $numero_p = rand(0,2000000);
                 $stmt = $coon->prepare($sql);
-                $stmt->bindParam(':pedido',$pedido);
-                $stmt->bindParam(':forma_p',$forma_p);
-                $stmt->bindParam(':endereco',$endereco);
-                $stmt->bindParam(':numero_p',$numero_p);
-                $stmt->bindParam(':data_fim',$data_fim);
+                $stmt->bindParam(':nome',$nome);
+                $stmt->bindParam(':forma_p',$setor);
+                $stmt->bindParam(':cnpj',$cnpj);
+                $stmt->bindParam(':data_de_envio',$data_de_envio);
+                $stmt->bindParam(':nota_fiscal_num',$nota_fiscal_num);
+                $stmt->bindParam(':financeiro_num',$financeiro_num);
+                $stmt->bindParam(':financeiro_num',$financeiro_num);
+                $stmt->bindParam(':financeiro_num',$financeiro_num);
+                $stmt->bindParam(':financeiro_num',$financeiro_num);
             } else {
                 $sql = "
-                UPDATE pedidos SET pedido =:pedido, forma_de_pagamento =:forma_p, endereco_de_entrega =:endereco,numero_do_pedido =:numero_p
-                WHERE pk_pedidos = :pk_pedidos
+                UPDATE empenhos SET nome =:nome, setor =:setor, cnpj =:cnpj,data_de_envio =:data_de_envio,nota_fiscal_num =:nota_fiscal_num,financeiro_num=:financeiro_num,
+                WHERE pk_empenhos = :pk_empenhos
                 ";
                 $stmt = $coon->prepare($sql);
-                $stmt->bindParam(':pk_pedidos',$pk_pedidos);
-                $stmt->bindParam(':pedido',$pedido);
-                $stmt->bindParam(':forma_p',$forma_p);
-                $stmt->bindParam(':endereco',$endereco);
-                $stmt->bindParam(':numero_p',$numero_p);
+                $stmt->bindParam(':pk_empenhos',$pk_empenhos);
+                $stmt->bindParam(':nome',$nome);
+                $stmt->bindParam(':setor',$setor);
+                $stmt->bindParam(':cnpj',$cnpj);
+                $stmt->bindParam(':data_de_envio',$data_de_envio);
+                $stmt->bindParam(':nota_fiscal_num',$nota_fiscal_num);
+                $stmt->bindParam(':financeiro_num',$financeiro_num);
+                $stmt->bindParam(':data_de_envio',$data_de_envio);
+                $stmt->bindParam(':data_de_envio',$data_de_envio);
             }
 
             //executa inset ou update acima
